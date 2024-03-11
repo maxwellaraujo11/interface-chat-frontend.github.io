a700277d5b6a3b9c86fa65cd8a5b93f2c83f1e93
Maxwell Araujo <maxwellaraujo11@hotmail.com>
2024-03-11 13:42:44 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function openMobile() {
+    document.querySelector('.menu-mobile').style.display = "block"
+    document.querySelector('.open-menu').style.display = "none"
+}
+
+function closeMobile() {
+    document.querySelector('.menu-mobile').style.display = "none"
+    document.querySelector('.open-menu').style.display = "block"
+}
